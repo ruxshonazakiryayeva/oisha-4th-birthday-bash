d8862d6d967467a36ab0ab3373cbf9006b9e908b
Ruxshona <133767416+ruxshonazakiryayeva@users.noreply.github.com>
2026-08-24 10:41:21 +0500

    Create payment-config.ts

---
+++ src/lib/payment-config.ts
@@ -0,0 +1,6 @@
+export const PAYMENT_CONFIG = {
+  price: "20 000 so'm",           // haqiqiy narxingiz
+  cardNumber: "4916 9912 0274 3204", // o'zingizning karta raqamingiz
+  cardOwner: "RUXSHONA ZAKIRYAYEVA", // kartada yozilgan ism
+  freeViewLimit: 3,                 // buni ham o'zgartirsangiz bo'ladi (masalan 5 ta qilib)
+};
